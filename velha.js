c35552c6d1a49faa197eea366df707bbb5269ffd
VSF <47865897+HashiY@users.noreply.github.com>
2022-02-06 15:58:31 -0300

    Parte 5

--- velha.js
+++ velha.js
@@ -1,6 +1,19 @@
 var jogador, vencedor = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
 
+mudarJogador('X'); // vai iniciar com x
 
 function escolherQuadrado(id){
-    console.log(id);
+
+    var quadrado = document.getElementById(id);//pega elemento do html
+
+    quadrado.innerHTML = jogador; // modifica tudo dentro da div
+    quadrado.style.color = '#000';
+
 }
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
